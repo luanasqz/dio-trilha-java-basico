675a6ed703c36115d8ed2a995ba68be3f3d406b8
Luana Sodário Queiroz <luanasqz@yahoo.com>
2023-03-08 23:27:06 -0300

    comentário e documentação

---
+++ sistema-smart-tv/src/SmartTV.java
@@ -0,0 +1,39 @@
+public class SmartTV {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void diminuirVolume(){
+        System.out.println("Diminuindo o volume para: " + volume);
+        //volume = volume - 1;
+        volume--;
+    }
+
+    public void aumentarVolume(){
+        System.out.println("Aumentando o volume para: " + volume);
+        //volume = volume + 1;
+        volume ++;
+    }
+
+    public void ligar(){
+        ligada = true;
+
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+    
+}
